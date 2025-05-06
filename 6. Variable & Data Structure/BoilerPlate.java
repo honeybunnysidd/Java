42e08c9de75752bfb5bb5c86ee86d567b4c016b6
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-06 09:22:37 +0530

    initial file

---
+++ 6. Variable & Data Structure/BoilerPlate.java
@@ -0,0 +1,9 @@
+//Boilerplate code
+
+public class BoilerPlate{
+
+    public static void main(String args[]){
+
+    }
+
+}
---
+++ 6. Variable & Data Structure/Output.java
@@ -0,0 +1,9 @@
+//Output in Java
+
+public class Output (){
+    
+    public static void main(String args[]){
+        
+        System.out.print("Hello Developers")
+    }
+}
