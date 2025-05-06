8e9f30042e8fbcefe9eb1fa74513eedeba298096
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-07 03:33:16 +0530

    TypeCasting

--- 6. Variable & Data Types/TypeCasting.java
+++ 6. Variable & Data Types/TypeCasting.java
@@ -2,14 +2,17 @@ public class TypeCasting{
     public static void main(String args[]){
 
 
-// Type Casting --------------------------------
-    // loss of data ---
-    // int marks = (int)(99.99f)
+// Type Casting (Explicit or Narrowing Conversion -Java not allowed, we do it forcefully)--------------------------------
+    // lossy Conversion ---loss of data
 
-    float a = 25.0;
-    int b = a;
+    float a = 27.10f;
+    // int b = a;       //Type casting Error
+    int b = (int) a;     //explicit converion with loss of data
     System.out.println(b);
 
+    char ch = 's';
+    int c = ch;  //Store its ASCII value (TypeConversion because it auto convert char to int)
+    System.out.println(c);
 
     }
 }
