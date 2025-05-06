b078007e824642326de67dc35ca1d00e3686c3f3
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-06 09:34:44 +0530

    JavaBasics

---
+++ 6. Variable & Data Structure/JavaBasics.java
@@ -0,0 +1,13 @@
+//Boilerplate code
+
+//Output in Java
+
+public class JavaBasics{
+    
+    public static void main(String args[]){
+        
+        System.out.println("Hello Developers");
+        System.out.println("Hello Developers");
+        System.out.println("Hello Developers");
+    }
+}
