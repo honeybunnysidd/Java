791d7ccf8e33dc4bf7a18b16fc033a86f938dfef
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-06 09:45:44 +0530

    JavaBasics

--- 6. Variable & Data Structure/JavaBasics.java
+++ 6. Variable & Data Structure/JavaBasics.java
@@ -1,13 +1,22 @@
 //Boilerplate code
 
-//Output in Java
-
 public class JavaBasics{
     
     public static void main(String args[]){
         
-        System.out.println("Hello Developers");
-        System.out.println("Hello Developers");
-        System.out.println("Hello Developers");
+//Output in Java
+        System.out.println("Hello Developers"); // print and cursor on next line
+        System.out.print("Hello Developers\n");  //same print and next line
+        System.out.print("Hello Developers"); // print
+
+// Print Pattern
+    System.out.print("\n");
+    System.out.println("* * * *");
+    System.out.println("* * * ");
+    System.out.println("* *");
+    System.out.println("*");
+
+
+
     }
 }
