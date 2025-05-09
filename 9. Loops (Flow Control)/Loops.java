4a5e6ea43f887eccead0f7befb9982efcccd8bbc
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-09 08:57:24 +0530

    Loops

--- 9. Loops (Flow Control)/Loops.java
+++ 9. Loops (Flow Control)/Loops.java
@@ -115,5 +115,85 @@ public class Loops {
     }while (i <= 10);
     */
 
+//Break Statement (To exit the loop) --------------------------------
+    /*
+    for(int i=1; i<=10;i++){
+        if(i==3){
+            break;
+        }
+        System.out.print(i + " ");
+    }
+    */
+
+    // Keep entering numbers until user enters a multiple of 10.
+    /*
+    Scanner scan = new Scanner(System.in);
+    while(true){
+        System.out.print("Enter multiple of 10 : ");
+        int number = scan.nextInt();
+        if(number % 10 == 0){
+        System.out.print(number + " is a multiple of 10");
+            break;
+        }
+    }
+    */
+
+// continue statement (to skip an iteration) --------------------------------
+    
+    //Example
+    /*
+    for (int i = 1; i <= 5; i++) {
+        if (i == 3) {
+            continue;
+        }
+        System.out.println(i);
+    }
+    */
+    
+    // Display all numbers entered by user except multiples of 10.
+    /*
+    Scanner scan = new Scanner(System.in);
+    do {
+        System.out.print("Enter your number : ");
+        int number = scan.nextInt();
+
+        if(number % 10 == 0 ){
+            continue;
+        }
+        System.out.println(number + " was your number");
+    }while(true);
+    */
+
+    // Check if a number is Prime or Not.
+    /*
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("Enter any number : ");
+    int number = scan.nextInt();
+    String isPrime = "";
+
+    if(number<=1){
+        isPrime = "The given number " + number + " is not Prime";
+
+    }else if(number == 2){
+        isPrime = "The given number " + number + " is Prime";
+    }else{
+        boolean flag = true;
+        for(int i=2; i<= number/2; i++){
+        if(number % i == 0){
+            flag = false;
+            break;
+        }
+       } 
+        if(flag){
+            isPrime = "The given number " + number + " is Prime";
+        }else{
+            isPrime = "The given number " + number + " is not Prime";
+        }
+    }
+    System.out.println(isPrime);
+    scan.close();
+    */
+
     }
 }
