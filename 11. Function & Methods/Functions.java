26035c289e599d13517cdf4017cfbe915e228dcd
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-11 16:08:40 +0530

    Functions

--- 11. Function & Methods/Functions.java
+++ 11. Function & Methods/Functions.java
@@ -22,11 +22,35 @@ public class Functions{
     return a+b;  //return int value (returnType -> int)
    }
 
+   //Swapping of two numbers
+   public static void swapping(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.print("a = " + a + " b = " + b );
+   }
+
+   //Factorial of a number
+   public static int factorial(int n){
+    int fact = 1;
+
+    if(n==0 || n==11){
+        return fact;
+    }else{
+
+    for(int i=1; i<=n; i++){
+        fact*=i;
+    }
+    return fact;
+    }
+   }
+
     public static void main(String arg[]){
         
         helloDev();     //Call the function
 
         //Sum of a & b
+        /*
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter a: ");
@@ -37,5 +61,21 @@ public class Functions{
 
         int sum = sum(a, b);  //arguments
         System.out.print("Sum = " + sum);
+        */
+
+        //Swapping of two numbers
+        /*
+        int a= 10;
+        int b = 27;
+        swapping(a, b);
+        */
+
+        //Factorial of a number
+        /*
+        int n = 7;
+        int factorial = factorial(n);
+        System.out.print(n+"! = " + factorial);
+        */
+
     }
 }
