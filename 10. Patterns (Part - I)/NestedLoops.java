3253af9ad392348f77e71539023848f1efd3947a
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-11 08:40:29 +0530

    NestedLoops

--- 10. Patterns (Part - I)/NestedLoops.java
+++ 10. Patterns (Part - I)/NestedLoops.java
@@ -1,18 +1,31 @@
 public class NestedLoops{
     public static void main(String args[]){
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
 
-        for(int i=4; i>=1; i--){
+        //Nested Loops
+
+        System.out.println("Star pattern");
+
+        //Outer Loop -> Showing Total number of Rows
+        for(int i=1; i<=6; i++){
+
+            //Inner loop -> What to print in a row
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
+        
+        //Print Inverted-Star pattern
+        int n=5;  // n= Total number of lines
+        for(int line=1; line<=n; line++){
+            
+            for(int star=1; star<= (n-line+1);star++){
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+        
         
     }
 }
