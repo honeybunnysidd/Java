8d623ec58c54ef5fbd56bb6aeb8f5b62909e16e5
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-12 11:12:12 +0530

    Overloading

--- 11. Function & Methods/Overloading.java
+++ 11. Function & Methods/Overloading.java
@@ -1,10 +1,23 @@
 public class Overloading{
 
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
     public static void main(String args[]){
 
     // Inbuilt v/s User Defined Methods --------------------------------
-    // User defined methods -> sum(), product(), factorial()
-    // Inbuilt methods -> sc.nextInt(), Math.sqrt(), Math.power(), Math.max() etc.
+        // User defined methods -> sum(), product(), factorial()
+        // Inbuilt methods -> sc.nextInt(), Math.sqrt(), Math.power(), Math.max() etc.
+
+    // Function Overloading --------------------------------
+        // Multiple function with the same name but 'different parameters'. (no of parameter, data type of parameter)
+        
+
+
 
     }
 
