ca5dc1f0da831b90333d0167f73e4137615771e6
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-15 13:23:41 +0530

    LinearSearch

---
+++ 13. Arrays I & II/LinearSearch.java
@@ -0,0 +1,18 @@
+public class LinearSearch{
+    public static void main(String args[]){
+
+    // Linear search --------------------------------
+
+    int number[] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int key = 10;
+
+    for(int i=0; i<number.length; i++){
+        if(number[i] = key){
+            System.out.println("Index of " + key + " is " + i);
+        }
+    }
+
+
+
+    }
+}
