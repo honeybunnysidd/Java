022ea17d9f14a89ba9bd44fa61e98734269b29a3
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-15 13:57:15 +0530

    LinearSearch

--- 13. Arrays I & II/LinearSearch.java
+++ 13. Arrays I & II/LinearSearch.java
@@ -1,18 +1,23 @@
 public class LinearSearch{
+
+    public static int linerSearch(int arr[], int key){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == key){
+             return i;  
+            }
+        }
+        return -1;  //item not found
+    }
+
     public static void main(String args[]){
 
     // Linear search --------------------------------
 
     int number[] = {2, 4, 6, 8, 10, 12, 14, 16};
-    int key = 10;
+    int key = 7;
 
-    for(int i=0; i<number.length; i++){
-        if(number[i] = key){
-            System.out.println("Index of " + key + " is " + i);
-        }
-    }
-
-
-
+    System.out.println("Index of " + key + " is " + linerSearch(number, key));
+    
+    
     }
 }
