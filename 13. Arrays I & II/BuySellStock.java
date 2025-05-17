fc093f7b051b810c7746a3f31c18a13a9a06c01a
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-05-18 01:23:19 +0530

    BuySellStock

--- 13. Arrays I & II/BuySellStock.java
+++ 13. Arrays I & II/BuySellStock.java
@@ -1,4 +1,23 @@
 public class BuySellStock{
-    
 
+    public static void buyAndSell(int arr[]){
+        int profit =0, buyPrice = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(buyPrice<arr[i]){ //profit
+               int curProfit = arr[i]-buyPrice;
+               profit = Math.max(curProfit, profit);
+            }else{
+                buyPrice = arr[i];
+            }
+        }
+        System.out.println("The maximum Profit : " + profit);
+    }
+
+    public static void main(String args[]){
+
+        int prices[] = {7,1,5,3,6,4};
+        buyAndSell(prices);
+
+    }
 }
