846189eeda96cc331b28ad644066265e47fafc9b
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-06-01 09:52:47 +0530

    ComareTo() method

---
+++ 16. Strings/CompareTo.java
@@ -0,0 +1,16 @@
+public class CompareTo{
+
+    //CompareTo() -> compare 2 string lexicographically.
+    public static void main(String args[]){
+        String fruits[] = {"apple", "mango", "zanana"};
+
+        String largestLexico = fruits[0]; //let
+        for(int i=0; i<fruits.length; i++){
+            if(fruits[i].compareTo(largestLexico)>0){
+                largestLexico = fruits[i];
+            }
+        }
+
+        System.out.println(largestLexico);
+    }
+}
