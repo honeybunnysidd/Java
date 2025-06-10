1de8321383b143fac49088e58b17f212bd218ae5
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-06-11 04:26:02 +0530

    Get and Set ith Bit

--- 17. Bit Manipulation/GetSet.java
+++ 17. Bit Manipulation/GetSet.java
@@ -6,6 +6,14 @@ public class GetSet{
        System.out.println("Set the bit : " +  setIthBit(10,2));
        
        System.out.println("Clear the bit : " +  clearIthBit(10,1));
+       
+       System.out.println("Update the bit : " +  updateIthBit(10,2,1));
+
+       System.out.println("Clear Multiple bits : " +  clearMultipleBits(15,2));
+
+       System.out.println("Clear ith to jth bits : " +  clearIthToJthBits(10,2,4));
+
+
     }
 
     //Get the ith bit
@@ -31,5 +39,34 @@ public class GetSet{
 
         return (n & bitMask) ;
     }
+
+    //Update the ith bit
+    public static int updateIthBit(int n, int i, int newBit){
+
+        if(newBit == 0){
+            return clearIthBit(n, i);
+        }else{
+            return setIthBit(n, i);
+        }
+    }
+
+    //Clear the ith bits (multiple i to last)
+    public static int clearMultipleBits(int n, int i){
+        int bitMask = (~0)<<i;
+
+        return n & bitMask;
+    }
+
+    //Clear the ith bits (multiple i to j)
+    public static int clearIthToJthBits(int n, int i, int j){
+        int a = (~0)<<(j+1);
+        int b = (1<<i) - 1;
+
+        int bitMask = a|b;
+
+        return n & bitMask;
+        
+    }
+
 
 }
